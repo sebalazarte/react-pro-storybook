501e92047f2913c257fda9c70ffe29b66d0bf628
Andres Sebastian Lazarte <sebalazarte@Andress-MacBook-Air.local>
2022-08-28 11:50:38 -0300

    propiedades en los controles

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,16 +1,42 @@
 import './MyLabel.css';
 
 export interface MyLabelProps {
+    /**
+     * text that is shown inside the label
+     */
     label?: string;
-    size?: 'normal' | 'h1' | 'h2' | 'h3'
+    /**
+     * is used to select the size of the label
+     */
+    size?: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Capitalize the text
+     */
+    allCaps?: boolean;
+    /**
+     * Set the color of label
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Custom color to use
+     */
+    fontColor?: string;
 }
 
-export const MyLabel = ({ label = 'no label', size = 'normal' }: MyLabelProps) => {
+export const MyLabel = ({
+    label = 'no label',
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
+    fontColor,
+    ...props }: MyLabelProps) => {
     return (
         <span
-            className={`${size}`}
+            className={['label', {size}, `text-${color}`].join(' ')}
+            style={{ color: fontColor }}
+            {...props}
         >
-            {label}
+            {allCaps ? label.toUpperCase() : label}
         </span>
     )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -3,18 +3,50 @@ import { MyLabel } from "../../components/MyLabel";
 
 export default {
     title: 'UI/MyLabel',
-    component: MyLabel
+    component: MyLabel,
+    argTypes: {
+        color: { control: 'select' },
+        size: { control: 'select' },
+        fontColor: { control: 'color' }
+    }
 } as ComponentMeta<typeof MyLabel>
 
-const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args}/> 
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />
 
 export const Basic = Template.bind({});
 Basic.args = {
     label: 'Ejemplo',
-    size: 'h1'
+    size: 'h1',
+    allCaps: false
 }
 
 export const AllCaps = Template.bind({});
+AllCaps.args = {
+    label: 'Solo mayuscula',
+    size: 'normal',
+    allCaps: true
+}
+
 export const Secondary = Template.bind({});
+Secondary.args = {
+    label: 'Color secundario',
+    size: 'normal',
+    color: 'secondary'
+}
+
+
+export const Terceary = Template.bind({});
+Terceary.args = {
+    label: 'Color ternario',
+    size: 'normal',
+    color: 'tertiary'
+}
+
+export const FontColor = Template.bind({});
+FontColor.args = {
+    label: 'Color personalizado',
+    size: 'normal',
+    fontColor: '#5517ac'
+}
 
 
